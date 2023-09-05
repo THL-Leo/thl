14d7a8cb23ce9ee863ac9aa135740773820bceae
Leo Lee <leolee901115@gmail.com>
2023-09-05 16:19:33 -0700

    added vercel/analytics package

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,14 @@ import '../styles/globals.css';
 import type {AppProps} from 'next/app';
 import {ThemeProvider} from 'next-themes';
 import {StyledEngineProvider} from '@mui/material/styles';
+import {Analytics} from '@vercel/analytics/react';
 
 export default function Site({Component, pageProps}: AppProps) {
   return (
     <StyledEngineProvider injectFirst>
       <ThemeProvider defaultTheme='system'>
         <Component {...pageProps} />
+        <Analytics />
       </ThemeProvider>
     </StyledEngineProvider>
   );
